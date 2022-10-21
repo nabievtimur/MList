b6dfa6328df3d5f17a0a48bf6290934ee231db97
Тимур Набиев <eragon96@inbox.ru>
2022-10-21 19:40:37 +0300

    Some fix

--- Forms/TableForm/TableFormOrder.Designer.cs
+++ Forms/TableForm/TableFormOrder.Designer.cs
@@ -153,6 +153,7 @@
             this.button3.TabIndex = 12;
             this.button3.Text = "Ок";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button4
             // 
@@ -162,6 +163,7 @@
             this.button4.TabIndex = 13;
             this.button4.Text = "Отмена";
             this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // TableFormOrder
             // 
--- Forms/TableForm/TableFormOrder.cs
+++ Forms/TableForm/TableFormOrder.cs
@@ -191,5 +191,15 @@ namespace MList.Forms.TableForm
             }
             this.UpdatePickedGunGrid();
         }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
