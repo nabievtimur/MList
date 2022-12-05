052092f9d3c2bdfe71e9bb787cd0420eb69c3ad4
Ivan Sinyavsky <ivan.godev@yandex.ru>
2022-12-06 00:50:17 +0300

    x

--- src/Printer/MsWord.cs
+++ src/Printer/MsWord.cs
@@ -2,6 +2,15 @@ namespace MList.Printer
 {
     public class MsWord
     {
+        // static
+        private static MsWord instance;
+        public static MsWord getInstance()
+        {
+            if (instance == null)
+                instance = new MsWord();
+            return instance;
+        }
+        
         
     }
 }
