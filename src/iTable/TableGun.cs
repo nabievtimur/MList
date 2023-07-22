a75b3487d7f9f8258ba54948ff90ea86edd9c799
Ivan Sinyavsky <ivan.s@dev.izibook.rus>
2023-07-22 21:24:30 +0300

    storageGetCurrentByEmployee()

--- src/iTable/TableGun.cs
+++ src/iTable/TableGun.cs
@@ -11,14 +11,17 @@ namespace MList.Storage.Table
             this.VisibleTableName = "Оружие";
             this.StorageTableName = "guns";
         }
+
         public override iContainer getAssociatedContainer()
         {
             return new ContainerGun();
         }
+
         public override iContainer getAssociatedContainer(DataGridViewRow row)
         {
             return new ContainerGun(row);
         }
+
         public override void gridInit(DataGridView table)
         {
             base.gridInit(table);
@@ -27,25 +30,30 @@ namespace MList.Storage.Table
             table.Columns.Add("number", "Номер");
             table.Columns.Add("ammo", "Патроны");
         }
+
         public override void storageAdd(iContainer container)
         {
             SqLite.exec(
                 "INSERT INTO " + this.StorageTableName + " (brand, series, number, ammo) " +
-                    "VALUES (@brand, @series, @number, @ammo)",
+                "VALUES (@brand, @series, @number, @ammo)",
                 container.storageFillParameterCollection,
                 "Add new gun.");
         }
+
         public override void storageUpdate(iContainer container)
         {
             SqLite.exec(
-                "UPDATE " + this.StorageTableName + " SET brand = @brand, series = @series, number = @number, ammo = @ammo WHERE id = @id",
+                "UPDATE " + this.StorageTableName +
+                " SET brand = @brand, series = @series, number = @number, ammo = @ammo WHERE id = @id",
                 container.storageFillParameterCollectionWithId,
                 "Update gun.");
         }
+
         public override void storageDelete(DataGridViewRow row)
         {
             storageDelete(new ContainerGun(row));
         }
+
         public override ContainerCollection<iContainer> storageGet()
         {
             return new ContainerCollection<ContainerGun>(SqLite.execGet(
@@ -53,30 +61,44 @@ namespace MList.Storage.Table
                 dFillerEmpty,
                 "Read " + this.StorageTableName + ".")).downCast();
         }
+
         public override ContainerCollection<iContainer> storageGet(string search)
         {
             return new ContainerCollection<ContainerGun>(SqLite.execGet(
                 "SELECT g.id, g.brand, g.series, g.number, g.ammo FROM " + this.StorageTableName + " g " +
-                    "WHERE g.brand LIKE @like OR g.series LIKE @like OR g.\"number\" LIKE @like or g.ammo LIKE @like " +
-                    "ORDER BY g.brand;",
+                "WHERE g.brand LIKE @like OR g.series LIKE @like OR g.\"number\" LIKE @like or g.ammo LIKE @like " +
+                "ORDER BY g.brand;",
                 (SqliteCommand command) => dFillerSearcher(command, search),
                 "Read " + this.StorageTableName + ".")).downCast();
         }
+
         public ContainerCollection<ContainerGun> storageGetCurrent(long MMlistId)
         {
             return new ContainerCollection<ContainerGun>(SqLite.execGet(
                 "SELECT gn.id, gn.brand, gn.series, gn.number, gn.ammo FROM " + this.StorageTableName + " AS gn " +
-                    "JOIN mlist_gun mg ON gn.id = mg.gun_id WHERE mg.mlist_id = @mlist_id",
+                "JOIN mlist_gun mg ON gn.id = mg.gun_id WHERE mg.mlist_id = @mlist_id",
                 (SqliteCommand command) => dFillerSCurrent(command, MMlistId),
                 "Get " + this.StorageTableName + " by mlist."));
         }
+
         public ContainerCollection<ContainerGun> storageGetCurrentByOrder(long mOrderId)
         {
             return new ContainerCollection<ContainerGun>(SqLite.execGet(
                 "SELECT gn.id, gn.brand, gn.series, gn.number, gn.ammo FROM " + this.StorageTableName + " AS gn " +
-                    "JOIN order_gun og ON gn.id = og.gun_id WHERE og.order_id = @order_id",
+                "JOIN order_gun og ON gn.id = og.gun_id WHERE og.order_id = @order_id",
                 (SqliteCommand command) => { command.Parameters.Add(new SqliteParameter("@order_id", mOrderId)); },
                 "Get " + this.StorageTableName + " by order."));
+        }
+
+        public ContainerCollection<ContainerGun> storageGetCurrentByEmployee(long mEmployeeId)
+        {
+            return new ContainerCollection<ContainerGun>(SqLite.execGet(
+                "SELECT gn.id, gn.brand, gn.series, gn.number, gn.ammo FROM " + this.StorageTableName + " AS gn " +
+                "JOIN order_gun og ON og.gun_id = gn.id " +
+                "JOIN orders o ON o.id = og.order_id " +
+                "WHERE o.employee_id = @employee_id",
+                (SqliteCommand command) => { command.Parameters.Add(new SqliteParameter("@employee_id", mEmployeeId)); },
+                "Get " + this.StorageTableName + " by employee."));
         }
     }
 }
